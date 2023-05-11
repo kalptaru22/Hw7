87020a89ab3ffe235c2330d586755fd49c1fae9a
kalptaru22 <pmaulik804@gmail.com>
2023-05-11 23:05:06 +0100

    midweek

---
+++ Hw_7/src/Mid_11.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Mid_11 {
+    /*Write a program to print a string entered by user.*/
+    public static void main(String[] args) {
+        pri();
+    }
+    public static void pri(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter a String");
+        String y= sc.next();
+        System.out.println("Enterd String is : " + y);
+
+    }
+}
---
+++ Hw_7/src/Mid_12.java
@@ -0,0 +1,11 @@
+public class Mid_12 {
+    /*Write a program to find the length of the string "refrigerator".
+     */
+    public static void main(String[] args) {
+        length();
+    }
+    public static void length(){
+        String s = "refrigerator";
+        System.out.println(s.length());
+    }
+}
